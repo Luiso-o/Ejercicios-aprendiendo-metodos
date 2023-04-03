c80b8aa3d180eed27efbdeca8c9be2d8ea72e223
Luis Trujillo <128043647+Luiso-o@users.noreply.github.com>
2023-04-03 11:35:03 +0200

    Add files via upload
    El programa Arroja una serie de datos random a partir del numero que el usuario ingresa, es un ejercicio para practicar algunos metodos matemáticos y jugar un poco

---
+++ DatosDeUnNumero.java
@@ -0,0 +1,94 @@
+package M_7_2;
+
+import java.util.Scanner;
+
+public class DatosDeUnNumero {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner input=new Scanner(System.in);
+
+		System.out.println("Introduce un numero que sea mayor o igual a 10");
+		int numero=input.nextInt();
+		
+		int fibonacci=fibonacci(numero);
+		
+		long sumarNumeros=sumadigitos(fibonacci);
+		
+		System.out.println("\nLa suma de los digitos de " + fibonacci + " es " + sumarNumeros);
+		
+		long operacion=esPrimo(sumarNumeros);
+		
+		int cuatroNumeros=tomaUltimosDigitos(operacion);
+		
+		System.out.println("Los ultimos digitos del numero anterior son: " + cuatroNumeros);
+		
+		bisiesto(cuatroNumeros);
+		
+		
+	}
+	
+	static int fibonacci(int numero) {
+		
+		 int a = 0, b = 1, c;
+	     System.out.print("La sucesion de Fibonacci de "+ numero + " es " + a + " " + b);
+	      
+	      for (int i = 2; i < numero; i++) {
+	         c = a + b;
+	         System.out.print(" " + c);
+	         a = b;
+	         b = c;
+	      }
+	      
+	      return b;
+	    
+	}
+	
+	static long esPrimo(long factorial) {
+	    
+		long operacion;
+		if (factorial % 2==0) {
+			operacion=factorial*factorial;
+		    System.out.println(factorial + " es un numero par,\n" 
+		    		+ factorial + " X " + factorial +" = "+ operacion);
+		    return operacion;
+		    
+		  }else {
+			  operacion=factorial*factorial*factorial;
+			  System.out.println(factorial + " es un numero primo,\n"
+					  + factorial + " X " + factorial + " X " + factorial + " = " + operacion);  
+			  return operacion;
+		  }
+		
+		}
+	
+	static int tomaUltimosDigitos(long numero) {
+		return  (int) (numero % 10000L);
+	}
+	
+	static void bisiesto(int cuatroNumeros) {
+		
+		if((cuatroNumeros % 4 == 0 && cuatroNumeros % 100 != 0) || cuatroNumeros % 400 == 0) {
+			System.out.println( cuatroNumeros + " es un año bisiesto ");
+
+		}else {
+			System.out.println( cuatroNumeros + " no es un año bisiesto ");
+		
+		}
+		
+	}
+	
+	static int sumadigitos(long numero) {
+		
+		int suma=0;	
+		while (numero != 0) {
+            int digito = (int) (numero % 10); // obtener el último dígito del número
+            suma += digito; // sumar el dígito al total
+            numero /= 10; // eliminar el último dígito del número
+		}
+		
+		return suma;
+		
+	}
+	
+}
